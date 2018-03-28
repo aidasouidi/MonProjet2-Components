7ee15767aac6d5a66f5497e1e56fe12a7d41f46f
Aida <souidi.aida90@gmail.com>
2018-03-28 16:01:19 +0100

    Ajout du routage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
@@ -7,17 +8,27 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProductsService } from './services/products.service';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
+const routes = [
+  {path: '', component: WelcomeComponent},
+  {path: 'list', component: ProductsListComponent},
+  {path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ProductsListComponent,
-    FooterComponent
+    FooterComponent,
+    WelcomeComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
